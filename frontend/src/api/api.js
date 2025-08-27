593f0bf04ddb2768e904d04cb2d6125857a04e7a
hiselvatest2026-ux <hiselvatest2026@gmail.com>
2025-08-27 07:55:35 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 export const getPurchases = () => axios.get(`${API_URL}/purchases`);
 export const createPurchase = (data) => axios.post(`${API_URL}/purchases`, data);
 export const updatePurchase = (id, data) => axios.put(`${API_URL}/purchases/${id}`, data);
